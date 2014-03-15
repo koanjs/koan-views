2672f2c736675bd8f0ff06385151b8182e8e1afe
Ruslan Bredikhin <rus.brn@gmail.com>
2014-03-15 12:08:40 -0400

    Some tests.

--- test/index.test.js
+++ test/index.test.js
@@ -9,9 +9,42 @@ var path = require('path');
 var views = require('../');
 
 describe('View renderer', function() {
-  it('should render layoutless views');
+  it('should render layoutless views', function(done) {
+    var renderer = views(
+      path.join(process.cwd(), 'test', 'fixtures', 'views'),
+      { default: 'ejs' }
+    );
 
-  it('should render views with default layout when specified');
+    var template = renderer('view', { msg: 'Hello, World!' });
+
+    template(function(err, html) {
+      if (err)
+        return done(err);
+
+      html.replace(/\n$/, '').should.be.eql('Hello, World!');
+
+      done();
+    });
+  });
+
+  it('should render views with default layout when specified', function(done) {
+    var renderer = views(
+      path.join(process.cwd(), 'test', 'fixtures', 'views'),
+      { default: 'ejs', defaultLayout: 'layout' }
+    );
+
+    var template = renderer('view', { msg: 'Hello, World!' });
+
+    template(function(err, html) {
+      if (err)
+        return done(err);
+
+      html.should.containEql('Hello, World!');
+      html.should.containEql('From the default layout: ');
+
+      done();
+    });
+  });
 
   it('should render views with default layout overriden');
 
